f9360aa85749326750ad09a52d84a31588f092ad
shashvatshukla <shashvatshukla@gmail.com>
2014-06-16 13:18:27 +0800

    Changed lattice to a list of edges instead of a bunch of arrays.

--- Edge.py
+++ Edge.py
@@ -1,8 +1,21 @@
 import wx
 
-
 class Edge():
 
-	start = 0
-	end = 0
-	weight = 0.0
+    nodelbl1 = None
+    nodelbl2 = None
+    wordlbl = None
+    latticeID = None
+    langModelScore = None
+    acousticModelScore = None
+    frameAlignment = None
+
+    def __init__(self,node1,node2,word,ID,lmscore,amscore,alignment):
+        self.nodelbl1 = node1
+        self.nodelbl2 = node2
+        self.wordlbl = word
+        self.latticeID = ID
+        self.langModelScore = lmscore
+        self.acousticModelScore = amscore
+        self.frameAlignment = alignment
+
--- Lattice.py
+++ Lattice.py
@@ -1,48 +1,67 @@
 import wx
 import numbers
+import Edge
+
+#
+# nodelbl1 = []
+# nodelbl2 = []
+# worldlbl = []
+# latticeID = []
+# langModelScore = []
+# acousticModelScore = []
+# frameAlignment = []
+
+
+edgeList = []
 
 class Lattice():
-	#-- TOFIX : What data structure should you use for this - BP
+    # -- TOFIX : What data structure should you use for this - BP
 
-	def __init__(self):
-		self.edgeList = []
+    def __init__(self):
+        self.edgeList = []
 
-	def parseFile(self, name):
-		"Parses lattice file and stores data"
-		i = 0
-		j = 0
-		nodelbl1 = []
-		nodelbl2 = []
-		worldlbl = []
-		latticeID = []
-		langModelScore = []
-		acousticModelScore = []
-		frameAlignment = []
-		path = "doc/" + name
-		fo = open(path, "r")
-		for line in fo:
-			if i == 0:
-				latticeID.append(line)
-			elif line == '\n':
-				i = -1
-			else:
+    def parseFile(self, name):
+        "Parses lattice file and stores data"
+        i = 0
+        j = 0
 
-				# This ignores the values at the end of the lattice
-				if line.split(" ")[0].isdigit():
-					nodelbl1.append(int(line.split(" ")[0]))
 
-				if line.split(" ")[1].isdigit():
-					nodelbl2.append(int(line.split(" ")[1]))		
+        # temp variables to create edge
+        nodelbl1 = None
+        nodelbl2 = None
+        wordlbl = None
+        latticeID = None
+        langModelScore = None
+        acousticModelScore = None
+        frameAlignment = None
 
-				if not line.split(" ")[2] == '\n':
-					worldlbl.append(line.split(" ")[2])
-					langModelScore.append(float(line.split(" ")[3].split(",")[0]))
-					acousticModelScore.append(float(line.split(" ")[3].split(",")[1]))
-					# This removes blank spaces and casts all the elements of the list to int before appending 
-					frameAlignment.append([map(int, [x for x in line.split(" ")[3].split(",")[2].split("_") if x])])
-					# frameAlignment is a 2d list
-			i+=1	
-		fo.close();
+        path = "doc/" + name
+        fo = open(path, "r")
+        for line in fo:
+            if i == 0:
+                latticeID = line
+            elif line == '\n':
+                i = -1
+            else:
+
+                # This ignores the values at the end of the lattice
+                if line.split(" ")[0].isdigit():
+                    nodelbl1 = int(line.split(" ")[0])
+
+                if line.split(" ")[1].isdigit():
+                    nodelbl2 = int(line.split(" ")[1])
+
+                if not line.split(" ")[2] == '\n':
+                    wordlbl = line.split(" ")[2]
+                    langModelScore = float(line.split(" ")[3].split(",")[0])
+                    acousticModelScore = float(line.split(" ")[3].split(",")[1])
+                    # This removes blank spaces and casts all the elements of the list to int before appending
+                    frameAlignment = [map(int, [x for x in line.split(" ")[3].split(",")[2].split("_") if x])]
+                    # frameAlignment is a 2d list
+            i += 1
+            edge = Edge.Edge(nodelbl1,nodelbl2,wordlbl,latticeID,langModelScore,acousticModelScore,frameAlignment)
+            self.edgeList.append(edge)
+        fo.close()
 
 
 
