ad72da4e252bc72080e8002ee56894320ef605c7
shashvatshukla <shashvatshukla@gmail.com>
2014-06-19 10:46:06 +0800

    Fixed Segmenter to upload to a given path from a given file

--- Segmenter.py
+++ Segmenter.py
@@ -2,53 +2,55 @@ from pydub import AudioSegment
 import vad
 import os
 import struct
+class Segmenter():
 
-infile = "C:\Users\oem\Desktop\hi.wav"
-i=0
-temp = None
+    infile = None
+    outpath = None
+    temp = None
 
-def __init__(input):
-    infile=input
+    def __init__(self,inputfile, outputpath):
+        self.infile=inputfile
+        self.outpath = outputpath
 
-def splitAudio(a, b,infile,outfile):
-    "splits the audio file from t=a to t=b milliseconds"
-    #path = "/Users/oem/Desktop/Segment"+str(++i)+".wav"
-    #audio = AudioSegment.from_wav('C:\\Users\\oem\\Desktop\\test.wav')
-    audio = AudioSegment.from_wav(infile)
-    audio_segment = audio[a:b]
-    audio_segment.export(outfile,format='wav')
+    def splitAudio(self,a, b,infile,outfile):
+        "splits the audio file from t=a to t=b milliseconds"
+        #path = "/Users/oem/Desktop/Segment"+str(++i)+".wav"
+        #audio = AudioSegment.from_wav('C:\\Users\\oem\\Desktop\\test.wav')
+        audio = AudioSegment.from_wav(infile)
+        audio_segment = audio[a:b]
+        audio_segment.export(outfile,format='wav')
 
+    def segmentAudio(self):
+        myvad = vad.VAD()
+        fin = open(os.path.abspath(self.infile),'rb')
+        fin.read(44)
+        block = fin.read(4000)
+        totsum = 0
+        chunk_cnt = 0
+        curr_chunk = []
+        myvad.Init()
+        i=0
+        while len(block) != 0:
+            samples = struct.unpack("=%ih" % (len(block)/2),block)
 
-
-# test the vad against a file
-myvad = vad.VAD()
-
-fin = open(os.path.abspath("C:\Users\oem\Desktop\hi.wav"),'rb')
-fin.read(44)
-block = fin.read(4000)
-
-totsum = 0
-chunk_cnt = 0
-curr_chunk = []
-myvad.Init()
-while len(block) != 0:
-    samples = struct.unpack("=%ih" % (len(block)/2),block)
-
-    events = myvad.DoVad(samples)
-
-
-    time = None
-    for framestamp,evt in events:
-        time=int(framestamp*10)
-        print "%.3f " % (time*0.001),
-        if evt == vad.VAD.SPSTART:
-            print " SPEECH START "
-            temp = time
-        else:
-            print " SPEECH END "
-            i+=1
-            splitAudio(int(temp),time,infile,"C:\\Users\\oem\\Desktop\\Segment"+str(i)+".wav")
+            events = myvad.DoVad(samples)
 
 
-    block = fin.read(4000)
+            time = None
+            for framestamp,evt in events:
+                time=int(framestamp*10)
+                print "%.3f " % (time*0.001),
+                if evt == vad.VAD.SPSTART:
+                    print " SPEECH START "
+                    temp = time
+                else:
+                    print " SPEECH END "
+                    i+=1
+                    self.splitAudio(temp,time,self.infile,self.outpath+"\\Segment"+str(i)+".wav")
 
+            block = fin.read(4000)
+
+out = os.path.dirname(vad.__file__)
+segmenter = Segmenter("C:\\Users\\oem\\Desktop\\test.wav",out)  # constructor for segmenter object
+                                                                # takes in absolute paths for inputfile and output directory
+segmenter.segmentAudio()  # does the segmentation
